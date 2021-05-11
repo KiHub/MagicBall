a48cb9a82b808d4d3f11313b25a0919a27223013
Kirill Ki <kirillkalapov@gmail.com>
2021-05-11 08:54:11 +0300

    Next step

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+   
+    @IBOutlet weak var imegeView: UIImageView!
+    @IBOutlet weak var askButtonPressed: UIButton!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
 
