2f81fcc644c423915baee6723596d2b9ccea91c7
Kirill Ki <kirillkalapov@gmail.com>
2021-05-17 22:14:52 +0200

    space view

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    override func viewDidLoad() {
+              super.viewDidLoad()
+                 assignbackground()
+                
+            }
+
+      func assignbackground(){
+            let background = UIImage(named: "space")
+
+            var imageView1 : UIImageView!
+            imageView1 = UIImageView(frame: view.bounds)
+        imageView1.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView1.clipsToBounds = true
+            imageView1.image = background
+            imageView1.center = view.center
+            view.addSubview(imageView1)
+            self.view.sendSubviewToBack(imageView1)
+        }
+    
    
     @IBOutlet weak var imegeView: UIImageView!
     
@@ -19,6 +39,7 @@ class ViewController: UIViewController {
         print("Button got tapped")
         let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
         imegeView.image = ballArray.randomElement()
+        //print(ballArray[3])
       
         
     }
