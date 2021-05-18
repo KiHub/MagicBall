a3c2c4f827b58cc6dc0c20007b4b10d931d76338
Kirill Ki <kirillkalapov@gmail.com>
2021-05-18 11:03:48 +0200

    final

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
         print("Button got tapped")
         let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
         imegeView.image = ballArray.randomElement()
-        //print(ballArray[3])
+        
       
         
     }
